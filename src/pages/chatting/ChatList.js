c8be6041d831e080f331dbd8fac38f25b2c73c02
JaeHyeokee <jaehyeok817@naver.com>
2024-08-03 21:26:04 +0900

    이재혁_20240803_채팅리스트사이드바

--- src/pages/chatting/ChatList.js
+++ src/pages/chatting/ChatList.js
@@ -4,32 +4,83 @@ import '../../css/chatting/ChatList.css'
 const ChatList = () => {
     return (
         <>
-        <section className='chat-list'>
-        <div className='chat-list'>
-            채팅목록입니당1
-        </div>
-        <br/>
-        <div className='chat-list'>
-            채팅목록입니당2
-        </div>
-        <br/>
-        <div className='chat-list'>
-            채팅목록입니당3
-        </div>
-        <br/>
-        <div className='chat-list'>
-            채팅목록입니당4
-        </div>
-        <br/>
-        <div className='chat-list'>
-            채팅목록입니당4
-        </div>
-        <br/>
-        <div className='chat-list'>
-            채팅목록입니당4
-        </div>
-        <br/>
-        </section>
+            <section className='chat-list'>
+                <div className='chat-elem'>
+                    <div className='img-frame-pf'>
+                        <img className='profile-img' src='https://img2.joongna.com/common/Profile/Default/profile_f.png' alt='프로필'/>
+                    </div>
+                    <div className='chat-room-info'>
+                        <div className='nickname-and-time'>
+                            <p className='nickname'>커피빈#9970766</p>
+                            <p className='timestamp'>오후 8:02</p>
+                        </div>
+                        <p className='message-text'>ㅋㅋㅋㅋㅋㅋㅋ</p>
+                    </div>
+                    <div className='img-frame-pd'>
+                        <img className='product-img' src='https://img2.joongna.com/media/original/2024/07/29/1722255733881FAw_NXRYv.jpg?impolicy=thumb' alt='프로필'/>
+                    </div>
+                </div>
+                <div className='chat-elem'>
+                    <div className='img-frame-pf'>
+                        <img className='profile-img' src='https://img2.joongna.com/common/Profile/Default/profile_f.png' alt='프로필'/>
+                    </div>
+                    <div className='chat-room-info'>
+                        <div className='nickname-and-time'>
+                            <p className='nickname'>커피빈#9970766</p>
+                            <p className='timestamp'>오후 8:02</p>
+                        </div>
+                        <p className='message-text'>ㅋㅋㅋㅋㅋㅋㅋ</p>
+                    </div>
+                    <div className='img-frame-pd'>
+                        <img className='product-img' src='https://img2.joongna.com/media/original/2024/07/29/1722255733881FAw_NXRYv.jpg?impolicy=thumb' alt='프로필'/>
+                    </div>
+                </div>
+                <div className='chat-elem'>
+                    <div className='img-frame-pf'>
+                        <img className='profile-img' src='https://img2.joongna.com/common/Profile/Default/profile_f.png' alt='프로필'/>
+                    </div>
+                    <div className='chat-room-info'>
+                        <div className='nickname-and-time'>
+                            <p className='nickname'>커피빈#9970766</p>
+                            <p className='timestamp'>오후 8:02</p>
+                        </div>
+                        <p className='message-text'>ㅋㅋㅋㅋㅋㅋㅋ</p>
+                    </div>
+                    <div className='img-frame-pd'>
+                        <img className='product-img' src='https://img2.joongna.com/media/original/2024/07/29/1722255733881FAw_NXRYv.jpg?impolicy=thumb' alt='프로필'/>
+                    </div>
+                </div>
+                <div className='chat-elem'>
+                    <div className='img-frame-pf'>
+                        <img className='profile-img' src='https://img2.joongna.com/common/Profile/Default/profile_f.png' alt='프로필'/>
+                    </div>
+                    <div className='chat-room-info'>
+                        <div className='nickname-and-time'>
+                            <p className='nickname'>커피빈#9970766</p>
+                            <p className='timestamp'>오후 8:02</p>
+                        </div>
+                        <p className='message-text'>ㅋㅋㅋㅋㅋㅋㅋ</p>
+                    </div>
+                    <div className='img-frame-pd'>
+                        <img className='product-img' src='https://img2.joongna.com/media/original/2024/07/29/1722255733881FAw_NXRYv.jpg?impolicy=thumb' alt='프로필'/>
+                    </div>
+                </div>
+                <div className='chat-elem'>
+                    <div className='img-frame-pf'>
+                        <img className='profile-img' src='https://img2.joongna.com/common/Profile/Default/profile_f.png' alt='프로필'/>
+                    </div>
+                    <div className='chat-room-info'>
+                        <div className='nickname-and-time'>
+                            <p className='nickname'>커피빈#9970766</p>
+                            <p className='timestamp'>오후 8:02</p>
+                        </div>
+                        <p className='message-text'>ㅋㅋㅋㅋㅋㅋㅋ</p>
+                    </div>
+                    <div className='img-frame-pd'>
+                        <img className='product-img' src='https://img2.joongna.com/media/original/2024/07/29/1722255733881FAw_NXRYv.jpg?impolicy=thumb' alt='프로필'/>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
