e0e4506b7e34e697c90ddca25b773904cf4b7250
JaeHyeokee <jaehyeok817@naver.com>
2024-08-06 19:55:23 +0900

    이재혁_20240806_마이홈

--- src/pages/my/MyInformation.js
+++ src/pages/my/MyInformation.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const MyInformation = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <h1>MyInformation</h1>  
+        </>
     );
 };
 
--- src/pages/my/UnRegister.js
+++ src/pages/my/UnRegister.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const UnRegister = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <h1>UnREgister</h1>  
+        </>
     );
 };
 
