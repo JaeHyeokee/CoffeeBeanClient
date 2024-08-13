536a5a749cb60c68820e19d7f99d04b30314e281
JaeHyeokee <jaehyeok817@naver.com>
2024-08-13 17:26:13 +0900

    이재혁_20240813_마이페이지 DB 연결 및 필터

--- src/pages/my/MyHome.jsx
+++ src/pages/my/MyHome.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ProgressBar } from 'react-bootstrap';
 import Style from '../../css/my/MyHome.module.css';
+import { LoginContext } from '../../contexts/LoginContextProvider';
 
 const MyHome = () => {
+    const { userInfo } = useContext(LoginContext);
+
     return (
         <section className={Style.myHomeInfo}>
             <img className={Style.myHomeProfileImg} src='https://img2.joongna.com/common/Profile/Default/profile_f.png' alt='프로필'/>
             <div className={Style.myHomeProfileContent}>
                 <div>
-                    <span className={Style.nickNamePrint}>HelloKitty</span>
+                    <span className={Style.nickNamePrint}>{userInfo.userName}</span>
                     <p className={Style.oneLineIntro}>안녕하세요! 키티에요~</p>
                 </div>
                 <div>
                     <div className={Style.trustIndexInfo}>
                         <div className={Style.myTrustIndex}>
                             <p className={Style.myTrustIndexLabel}>신뢰지수</p>
-                            <p className={Style.myTrustIndexFigure}>550</p>
+                            <p className={Style.myTrustIndexFigure}>{userInfo.reliability}</p>
                         </div>
                         <p className={Style.maxTrustIndex}>1,000</p>
                     </div>
-                    <ProgressBar className={Style.trustIndexBar} now={550 / 10}/>
+                    <ProgressBar className={Style.trustIndexBar} now={userInfo.reliability / 10}/>
                 </div>
             </div>
         </section>
