e2bf1a61699c3a4cb80c9bf3c46a9b575f6c7d0b
JaeHyeokee <jaehyeok817@naver.com>
2024-08-14 15:21:51 +0900

    이재혁_20240814_마이페이지 수정2

--- src/pages/my/MyHome.jsx
+++ src/pages/my/MyHome.jsx
@@ -1,14 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ProgressBar } from 'react-bootstrap';
 import Style from '../../css/my/MyHome.module.css';
 import { LoginContext } from '../../contexts/LoginContextProvider';
 
 const MyHome = () => {
     const { userInfo } = useContext(LoginContext);
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setProgress((oldProgress) => {
+                if(oldProgress >= userInfo.reliability) {
+                    clearInterval(interval);
+                    return userInfo.reliability;
+                }
+                return Math.min(oldProgress + 10, userInfo.reliability);
+            });
+        }, 10);
+        return () => clearInterval(interval);
+    }, [userInfo.reliability]);
 
     return (
         <section className={Style.myHomeInfo}>
-            <img className={Style.myHomeProfileImg} src='https://img2.joongna.com/common/Profile/Default/profile_f.png' alt='프로필'/>
+            <img className={Style.myHomeProfileImg} src={'https://img2.joongna.com/common/Profile/Default/profile_f.png'} alt='프로필'/>
             <div className={Style.myHomeProfileContent}>
                 <div>
                     <span className={Style.nickNamePrint}>{userInfo.userName}</span>
@@ -18,11 +32,11 @@ const MyHome = () => {
                     <div className={Style.trustIndexInfo}>
                         <div className={Style.myTrustIndex}>
                             <p className={Style.myTrustIndexLabel}>신뢰지수</p>
-                            <p className={Style.myTrustIndexFigure}>{userInfo.reliability}</p>
+                            <p className={Style.myTrustIndexFigure}>{progress}</p>
                         </div>
                         <p className={Style.maxTrustIndex}>1,000</p>
                     </div>
-                    <ProgressBar className={Style.trustIndexBar} now={userInfo.reliability / 10}/>
+                    <ProgressBar className={Style.trustIndexBar} now={progress / 10}/>
                 </div>
             </div>
         </section>
