8d9c33057299f476fe19ee7f35920101bae46312
djand123 <djand123@naver.com>
2024-08-22 15:39:17 +0900

    최민혁_20240822_로그인창

--- src/pages/components/Kakao/KaKaoLogin.jsx
+++ src/pages/components/Kakao/KaKaoLogin.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import kakao from '../../../image/kakao_login2.png';
+import kakao from '../../../image/kakao_logo.png';
 import styles from '../../../css/my/LogIn.module.css';
 
 const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
 const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
 
-
 const KaKaoLogin = () => {
-
     const handleLogin = () => {
         const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
         window.location.href = kakaoAuthUrl;
     };
 
     return (
-        <>
-        {/* <button onClick={handleLogin}>
-            카카오 로그인
-        </button> */}
-        <img className={styles.kakobtn}
-        src={kakao} 
-        onClick={handleLogin}/>
-        </>
+        <button className={styles.kakaoButton} onClick={handleLogin}>
+            <img src={kakao} alt="kakao" className={styles.kakaoIcon} />
+            <span className={styles.kakaoText}>카카오톡 로그인</span>
+        </button>
     );
 };
 
