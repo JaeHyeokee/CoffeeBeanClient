ee46524206d39306f15292a557dc12236f785a84
djand123 <djand123@naver.com>
2024-08-23 09:04:27 +0900

    최민혁_20240823 로그인창

--- src/pages/my/LogIn.jsx
+++ src/pages/my/LogIn.jsx
@@ -3,15 +3,17 @@ import LoginForm from '../components/LoginForm';
 import KaKaoLogin from '../components/Kakao/KaKaoLogin';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import my from '../../image/MyIcon.svg';
 // import styles from '../../css/my/LogIn.module.css';
 
 const LogIn = () => {
     return (
         <>
+            <Header />
             <div className='container'>
                 <LoginForm/>
-                <KaKaoLogin/>
             </div>
+            <Footer />
         </>
 
     );
